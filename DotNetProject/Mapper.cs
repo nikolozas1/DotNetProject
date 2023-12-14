dcd22394c204660eafe179a85a257d1175f97b43
nikolozas1 <42701195+nikolozas1@users.noreply.github.com>
2023-12-14 23:59:57 +0400

    Update Mapper.cs

--- DotNetProject/Mapper.cs
+++ DotNetProject/Mapper.cs
@@ -5,6 +5,6 @@ namespace DotNetProject
     [Mapper]
     public partial class Mapper
     {
-        public partial Book DTOToBook(BookDTO car);
+        public partial Book DTOToBook(BookDTO book);
     }
 }
